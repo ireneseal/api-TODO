75024f51d7dd2e2d45f69b33ef583a3aff1ddd85
Irene <irene.sebastian.alarcon@gmail.com>
2024-02-15 12:48:47 +0100

    Entorno de pruebas de los métodos (GET, POST, PUT...)

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
+
 const servidor = express();
+
+servidor.use("/prueba", express.static("./pruebas_api"));
+
+servidor.get("/api-todo", (peticion, respuesta)=>{
+    respuesta.send("metodo GET")
+});
+
+servidor.post("/api-todo", async(peticion, respuesta)=>{
+    respuesta.send("metodo POST")
+});
+
+servidor.put("/api-todo", (peticion, respuesta)=>{
+    respuesta.send("metodo PUT")
+});
+
+servidor.delete("/api-todo", (peticion, respuesta)=>{
+    respuesta.send("metodo DELETE")
+});
+
+servidor.use((peticion, respuesta)=>{
+    respuesta.json({error : "not found"})
+}); //El .use es para todas las url que resten, que no sean ninguna de las anteriores
+
 
 
 servidor.listen(process.env.PORT);
